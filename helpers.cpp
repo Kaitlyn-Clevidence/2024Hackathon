dc10bf14407d28d24e71c0c297b465b57c88fc91
1105-Clevidence-Kaitlyn <143451703+1105-Clevidence-Kaitlyn@users.noreply.github.com>
2024-04-12 21:11:06 -0700

    Update helpers.cpp

--- helpers.cpp
+++ helpers.cpp
@@ -1,4 +1,5 @@
 #include "hand.h"
+#include "hand.h"
 /*
 drawCard()
     //take first card of the availible hand and add it to the hand of who ever needs to draw the card 
@@ -6,16 +7,25 @@ drawCard()
     //delete the previous first card in the availible hand 
     //decrement the number of cards in the availible hand 
 playCard()
-    //
+    //display chosen card
+    //add chosen card to end of discard hand 
+    //delete chosen card from the hand played from 
 displayPlayerHand()
 shuffleCards()
 playerTurn() 
+    //check if player has any valid cards (loop)
+        //if not, draw 1 card and switch to computer turn 
     //show player their hand
     //prompt for choice of card
     //validate card
+        //if invalid, prompt again
+    //playCard()
     - show the player their hand and allow them to pick one of their cards (ask for input of a character relating to each card) 
     - calls displayplayerhand and validate card and deletecardfromhand
-computerTurn() - loop through computer hand until a valid (validateCard) is found, then play that card. remove the card from the array (dma may be necessary here so we have a first card and last card pointer)
+computerTurn() - 
+    //loop through computer hand until a valid (validateCard) is found, then play that card. 
+    //remove the card from the array
+    //if no valid card is found, draw 1 card and switch turns 
 validateCard(lastCard)
     //make sure the card is either the number, color, or wild card
 
