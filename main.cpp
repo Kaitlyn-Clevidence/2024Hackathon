d5bb733c7b82fcb9654a1d62e9413dc5ea54a5ca
1101-Grills-Oren <134662565+1101-Grills-Oren@users.noreply.github.com>
2024-04-13 13:04:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main(){
     Hand available, discard, player, computer;
-    int userChoice, turn = 0;
-    bool win = false;
+    int userChoice;
+    
     cout << "UNO" << endl;
     do{
         cout << "Do you want to play a game (y/n)? " << endl;
@@ -11,12 +11,37 @@ int main(){
     }while(userChoice != 'y' && userChoice != 'n');
 
     //Populate the available hand
+    int strings[108];
+    int n=0;
+    for(int i=0;i<1;i++){
+        for(int j=0;j<4;j++){
+            strings[n]=(i*4+j);n++;
+        }
+    }
+    for(int i=1;i<11;i++){
+        for(int j=0;j<4;j++){
+            strings[n]=(i*4+j);strings[n+1]=(i*4+j);n+=2;
+        }
+    }
+    for(int i=11;i<16;i++){
+        for(int j=0;j<4;j++){
+            strings[n]=(i*4+j);n++;
+        }
+    }
+    RANDOIMIZE(strings,104);
+    /*for(int i=0;i<27;i++){
+        for(int j=0;j<4;j++){
+            displayCard((strings[i*4+j]),5*j+2,5*i+2);
+        }
+    }*/
+    cardStack(5,5,0);
+    cout<<"\e[25;25H";
+
     //Add 7 cards to both the player and computer hands
-    //Game logic and stuff :)
+    //Game logic and stuff
     do{
         if(turn%2==1){
             win = playerTurn(player, computer, discard, available, turn);
-            
         }
         if(turn%2 == 0){
             win = computerTurn(computer, player, discard, available, turn);
