429fbb31edfe10e3076404e0525819d97f781505
1105-Clevidence-Kaitlyn <143451703+1105-Clevidence-Kaitlyn@users.noreply.github.com>
2024-04-13 13:47:19 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main(){
     Hand available, discard, player, computer;
-    int userChoice, turn =0;
+    int turn =0;
     bool win;
+    char userChoice;
 
     int numCards = 1;
     bool isUnoCalled = false;
@@ -11,35 +12,54 @@ int main(){
     Hand availableCards;
     
     cout << "UNO" << endl;
-    char temp;
     do{
         cout << "Do you want to play a game (y/n)? " << endl;
+        cin >> userChoice;
+    }while(userChoice != 'y' && userChoice != 'Y' && userChoice != 'N' && userChoice != 'n');
+    if(userChoice == 'n' || userChoice == 'N'){
+        return 0;
+    }
 
-        cin >> temp;
-        userChoice=temp;
-    }while(userChoice != 'y' && userChoice != 'n');
-
+    cout<<"1"<<endl;
     //Populate the available hand
     int strings[108];
+    cout<<"2"<<endl;
     int n=0;
+    cout<<"3"<<endl;
     for(int i=0;i<1;i++){
+        cout<<"4"<<endl;
         for(int j=0;j<4;j++){
+            cout<<"5"<<endl;
             strings[n]=(i*4+j);n++;
         }
     }
+    cout<<"6"<<endl;
     for(int i=1;i<11;i++){
+            cout<<"7"<<endl;
+
         for(int j=0;j<4;j++){
+                cout<<"8"<<endl;
+
             strings[n]=(i*4+j);strings[n+1]=(i*4+j);n+=2;
         }
     }
+        cout<<"9"<<endl;
+
     for(int i=11;i<16;i++){
+            cout<<"10"<<endl;
+
         for(int j=0;j<4;j++){
+                cout<<"11"<<endl;
+
             strings[n]=(i*4+j);n++;
         }
     }
+        cout<<"12"<<endl;
+
     RANDOIMIZE(strings,104);
+    cout<<"ugh"<<endl;
     //actually input into the hand 
-    for(int i = 0; i < 108; i++){
+    for(int i = 0; i < 104; i++){
         Card temp(to_string(strings[i]));
         available.addCardToHand(temp);
     }
