ae8e370d706988c73e4fb02e0fb8f720db7c2543
1105-Clevidence-Kaitlyn <143451703+1105-Clevidence-Kaitlyn@users.noreply.github.com>
2024-04-13 10:58:19 -0700

    Add files via upload

--- helpers.cpp
+++ helpers.cpp
@@ -1,4 +1,5 @@
 #include "hand.h"
+#include <random>
 
 void drawCard(Hand current, Hand available){
     current.addCardToHand(*available.getFirstCard());
@@ -6,19 +7,43 @@ void drawCard(Hand current, Hand available){
 }
 
 //specialty cards 
-void checkSpecialtyCards(Card c, Hand opposite, Hand available){
-    if(c == wildvalues){
+bool checkSpecialtyCardsPlayer(Card c, Hand opposite, Hand available){
+    if(c.getValue()[2] == 'Y'){
         wild();
+        return true;
     }
-    if(c == skipvalues || c == reversevalues){
+    if(c.getValue()[2] == 'S' || c.getValue()[2] == 'R'){
         skip(opposite);
+        return true;
     }
-    if(c = p2values){
+    if(c.getValue()[2] = '+'){
         plus2(opposite, available);
+        return true;
     }
-    if(c == wildp4values){
+    if(c.getValue()[2] == 'W'){
         wildPlus4(opposite, available);
+        return true;
     }
+    return false;
+}
+bool checkSpecialtyCardsComputer(Card c, Hand opposite, Hand available){
+    if(c.getValue()[2] == 'Y'){
+        wildComputer();
+        return true;
+    }
+    if(c.getValue()[2] == 'S' || c.getValue()[2] == 'R'){
+        skip(opposite);
+        return true;
+    }
+    if(c.getValue()[2] = '+'){
+        plus2(opposite, available);
+        return true;
+    }
+    if(c.getValue()[2] == 'W'){
+        wildPlus4Computer(opposite, available);
+        return true;
+    }
+    return false;
 }
 void wild(){
     //prompt for color 
@@ -57,7 +82,7 @@ void wildPlus4(Hand other, Hand a){
     wild();
 }
 
-void playerTurn(Hand p, Hand discard, Hand available){
+void playerTurn(Hand p,Hand np, Hand discard, Hand available){
     Card* temp = p.getFirstCard();
     Card* test = discard.getLastCard();
     for(int i = 0; i < p.getNumCardsInHand(); i++){
@@ -68,40 +93,74 @@ void playerTurn(Hand p, Hand discard, Hand available){
                 cout<<"Which card would you like to play? ";
                 cin>>choice;
             }while(choice<1 || choice>p.getNumCardsInHand());
-            //getCardatIndex
-            if(validateCard())
+            temp = p.getCardAtIndex(choice-1);
+            if(validateCard(test, temp)){
+                if(!checkSpecialtyCardsPlayer(*temp, np, available)){
+                    playCard(temp);
+                }
+            }
             break;
-
         }
     }
     drawCard(p, available);
 }
-    //validate card
-        //if invalid, prompt again
-    //check if special card
-        //if special call according special function (skip&reverse have same function)
-    //playCard()
-    //show the player their hand and allow them to pick one of their cards (ask for input of a character relating to each card) 
-    //calls displayplayerhand and validate card and deletecardfromhand
-void playCard(Hand hand, Hand discard){
+
+void computerTurn(Hand c, Hand nc, Hand discard, Hand available){
+    Card* temp = c.getFirstCard();
+    Card* test = discard.getLastCard();
+    for(int i = 0; i < c.getNumCardsInHand(); i++){
+        if(validateCard(test, temp)){//test if any cards in the hand are valid 
+            //random from 1-getnumcardsinhand 
+            // Seed for random number generation
+            random_device rd;
+            mt19937 gen(rd());
+            // Define the distribution
+            uniform_int_distribution<> dis(1, c.getNumCardsInHand());
+            // Generate a random number
+            int randomNumber = dis(gen);
+            
+            temp = c.getCardAtIndex(randomNumber-1);
+            if(validateCard(test, temp)){
+                if(!checkSpecialtyCardsComputer(*temp, nc, available)){
+                    playCard(temp);
+                }
+            }
+            break;
+        }
+    }
+    drawCard(c, available);
+}  
+
+/*
+computerTurn() - 
+    //loop through computer hand until a valid (validateCard) is found,
+    //check if the card is special
+        //if so, call according function
+    //playFunction() 
+    //remove the card from the array
+    //if no valid card is found, draw 1 card and switch turns 
+playCard(card )
     //display chosen card
     //add chosen card to end of discard hand 
-    //delete chosen card from the hand played from
-    int cardChoice;
-    displayPlayerHand(hand);
-    do{
-        cout << "Pick one of your cards to play: " << endl;
-        cin >> cardChoice;
+    //delete chosen card from the hand played from 
+    //andrew is working on it 
+displayPlayerHand()
+    //loop through player cards and display each of them side by side - need values 
+shuffleCards()
+    //oren is working on it 
 
-        if(validateCard(discard.getLastCard() ,hand.getCardatIndex(userChoice - 1))){
-        discard.addCardToHand(hand.getCardatIndex(userChoice - 1));
-        hand.deleteCardFromHand(hand.getCardatIndex(userChoice - 1));
-        }
-        else{
-        cout << "Invalid card. Choose again." << endl;
-        }
-    }while(cardChoice < 0 && cardChoice > hand.getNumCardsInHand());
-    
+bool validateCard(lastCard, chosenCard)
+    //make sure the card is either the number (in value), color (in value), or wild card (is a set of values)
+
+skip()
+    whoever is playing plays again
+
+*/
+void playCard(Hand hand, Hand discard, int choice){
+    //display chosen card
+
+    discard.addCardToHand(hand.getCardatIndex(choice - 1));
+    hand.deleteCardFromHand(hand.getCardatIndex(choice - 1));
 }
 
 void displayPlayerHand(Hand hand){
@@ -112,46 +171,3 @@ void displayPlayerHand(Hand hand){
         currentCard = currentCard -> getNextCard();
     }
 }
-
-
-/*
-shuffleCards()
-    //oren is working on it 
-//playerTurn() 
-    for(int i )
-    //check if player has any valid cards (loop)
-        //if not, draw 1 card and switch to computer turn 
-    //show player their hand
-    //prompt for choice of card
-    //validate card
-        //if invalid, prompt again
-    //check if special card
-        //if special call according special function (skip&reverse have same function)
-    //playCard()
-    - show the player their hand and allow them to pick one of their cards (ask for input of a character relating to each card) 
-    - calls displayplayerhand and validate card and deletecardfromhand
-computerTurn() - 
-    //loop through computer hand until a valid (validateCard) is found,
-    //check if the card is special
-        //if so, call according function
-    //playFunction() 
-    //remove the card from the array
-    //if no valid card is found, draw 1 card and switch turns 
-bool validateCard(lastCard, chosenCard)
-    //make sure the card is either the number (in value), color (in value), or wild card (is a set of values)
-
-
-specialCards(Card){
-    if Card == wild values
-        wild()        
-    if card == skip or reverse values
-        skip()
-    if card == plus 2 values
-        plus 2()
-    if card == wildPlus4 values
-}
-
-skip()
-    whoever is playing plays again
-
-*/
