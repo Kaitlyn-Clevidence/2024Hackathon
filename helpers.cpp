fc9c5236a0e2344971e0eb768e70ae3876ac6f99
1105-Clevidence-Kaitlyn <143451703+1105-Clevidence-Kaitlyn@users.noreply.github.com>
2024-04-12 21:44:57 -0700

    Update helpers.cpp

--- helpers.cpp
+++ helpers.cpp
@@ -1,6 +1,7 @@
 #include "hand.h"
 #include "hand.h"
 /*
+*
 drawCard()
     //take first card of the availible hand and add it to the hand of who ever needs to draw the card 
     //reset the first card in the availible hand to the next card
@@ -21,21 +22,46 @@ playerTurn()
     //prompt for choice of card
     //validate card
         //if invalid, prompt again
+    //check if special card
+        //if special call according special function (skip&reverse have same function)
     //playCard()
     - show the player their hand and allow them to pick one of their cards (ask for input of a character relating to each card) 
     - calls displayplayerhand and validate card and deletecardfromhand
 computerTurn() - 
-    //loop through computer hand until a valid (validateCard) is found, then play that card. 
+    //loop through computer hand until a valid (validateCard) is found,
+    //check if the card is special
+        //if so, call according function
+    //playFunction() 
     //remove the card from the array
     //if no valid card is found, draw 1 card and switch turns 
 bool validateCard(lastCard)
     //make sure the card is either the number, color, or wild card
 
 
+specialCards(Card){
+    if Card == wild values
+        wild()        
+    if card == skip or reverse values
+        skip()
+    if card == plus 2 values
+        plus 2()
+    if card == wildPlus4 values
+}
 some functionality for specialty cards
-wild 
-skip & reverse have the same funtionality since it is two players 
-plus2 
-wild plus 4
+wild()
+    prompt for color 
+    change display card to just a solid color card of their choice 
+    change validation 
+skip()
+    whoever is playing plays again
+plus2() 
+    drawCard(other player hand)
+    drawCard(other player hand)
+wildPlus4()
+    drawCard(other player hand)
+    drawCard(other player hand)
+    drawCard(other player hand)
+    drawCard(other player hand)
+    wild()
 
 */
