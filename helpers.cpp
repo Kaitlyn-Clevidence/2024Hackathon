3e752d0f069112a35c1a72bfca17d8e6215a9614
1105-Clevidence-Kaitlyn <143451703+1105-Clevidence-Kaitlyn@users.noreply.github.com>
2024-04-12 21:23:37 -0700

    Update helpers.cpp

--- helpers.cpp
+++ helpers.cpp
@@ -11,7 +11,9 @@ playCard()
     //add chosen card to end of discard hand 
     //delete chosen card from the hand played from 
 displayPlayerHand()
+    //loop through player cards and display each of them side by side 
 shuffleCards()
+    //oren is working on it 
 playerTurn() 
     //check if player has any valid cards (loop)
         //if not, draw 1 card and switch to computer turn 
@@ -26,7 +28,14 @@ computerTurn() -
     //loop through computer hand until a valid (validateCard) is found, then play that card. 
     //remove the card from the array
     //if no valid card is found, draw 1 card and switch turns 
-validateCard(lastCard)
+bool validateCard(lastCard)
     //make sure the card is either the number, color, or wild card
+
+
+some functionality for specialty cards
+wild 
+skip & reverse have the same funtionality since it is two players 
+plus2 
+wild plus 4
 
 */
