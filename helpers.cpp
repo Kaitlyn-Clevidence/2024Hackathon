6f991e5cd9c73421de76c0550bf5ccd1aa56fc15
1101-Grills-Oren <134662565+1101-Grills-Oren@users.noreply.github.com>
2024-04-13 19:31:50 -0700

    Update helpers.cpp

--- helpers.cpp
+++ helpers.cpp
@@ -236,10 +236,44 @@ bool checkSpecialtyCardsComputer(Card c, Hand& opposite, Hand& available, int tu
     return false;
 }
 
-void playCard(Hand& hand, Hand& discard, int choice){
+void displayChat(){
+    for(int i=mewwo-1;i>mewwo-10&&i>-1;i--){
+        printf("\e[%d;%ldH\e[38;5;%dm",i-mewwo+15,115-mewo[i].length(),255-0*(mewwo-i));
+        cout<<mewo[i]<<"\e[m";
+    }
+};
+void playCard(Hand& hand, Hand& discard, int choice, string text){
+    int tempValue= stoi(hand.getCardAtIndex(choice - 1).getValue());
+    string color;
+    if(tempValue%4==0){color="R";}
+    else if((tempValue%4)==1){color="B";}
+    else if((tempValue%4)==2){color="G";}
+    else if((tempValue%4)==3){color="Y";}
+    string value;
+    
+    switch(getSecond(tempValue)){
+        case 0:value="0";break;
+        case 1:value="1";break;
+        case 2:value="2";break;
+        case 3:value="3";break;
+        case 4:value="4";break;
+        case 5:value="5";break;
+        case 6:value="6";break;
+        case 7:value="7";break;
+        case 8:value="8";break;
+        case 9:value="9";break;
+        case 10:value="Reverse";break;
+        case 11:value="Wild +4";break;
+        case 12:value="+2";break;
+        case 13:value="S";break;
+        case 14:value="Wild";break;
+    }
+    mewo[mewwo]=text+color+" "+value;
+    mewwo++;
     discard.addCardToHand(hand.getCardAtIndex(choice - 1));
     hand.deleteCardFromHand(choice - 1);
 }
+
 
 bool playerTurn(Hand& p, Hand& np, Hand& discard, Hand& available, int turn){
     Card* temp = p.getFirstCard();
